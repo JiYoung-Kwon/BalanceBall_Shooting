ec82548b5fa5a9fc7afc774cf98440a27bbd60e8
JJIYOM <57945918+JJIYOM@users.noreply.github.com>
2020-06-26 20:24:20 +0900

    Serial_Test & Shooting Range PROJECT

---
+++ Serial_Test/Assets/SerialTest.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System.Text;
+using System;
+using System.IO;
+
+/*
+ * BalanceBall 시리얼 통신 test 스크립트
+ */
+
+public class SerialTest : MonoBehaviour
+{
+    public GameObject Cube;
+
+    private SerialPort sp;
+    public string message;
+    
+    byte[] by = new byte[1];
+    string[] tempstr;
+    int[] data;
+
+    double Gyro_Angle_x = 0, Gyro_Angle_y = 0, Gyro_Angle_z = 0;
+    double tmp_angle_x=0, tmp_angle_y=0, tmp_angle_z=0;
+
+    void Start() //포트 연결
+    {
+        by[0] = 0x02;
+        string[] ports = SerialPort.GetPortNames();
+        foreach (string p in ports)
+        {
+            sp = new SerialPort(p, 115200, Parity.None, 8, StopBits.One); // 초기화
+
+            try
+            {
+                sp.Open(); // 프로그램 시작시 포트 열기
+
+                sp.Write(by, 0, 1); // 0x02 값을 받으면 데이터 출력 시작 --> by 배열 이용
+            }
+            catch (TimeoutException e) //예외처리
+            {
+                Debug.Log("timeout");
+                continue;
+            }
+            catch (IOException ex) //예외처리
+            {
+                Debug.Log("io exception");
+                continue;
+            }
+            Debug.Log("send message");
+            Debug.Log(p);
+            if (sp.ReadLine().Equals("")) continue;
+            else break;
+        }
+        Debug.Log(sp.ReadLine());
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (sp.IsOpen) //시리얼 포트가 열려있다면
+        {
+            string temp = sp.ReadLine(); //값 받아오는 거 (OK)            
+            tempstr = temp.Split(','); //자르는 거 (OK)
+            data = Array.ConvertAll(tempstr, int.Parse); //int로 변환
+
+            //for (int i = 0; i < data.Length; i++) Debug.Log(i + " = " + data[i]);
+
+            //data[n] 값으로 버튼 클릭 여부를 받아옴
+            if (data[0] == 1)
+                InputData.isLeftButtonCliked = true; 
+            else
+                InputData.isLeftButtonCliked = false;
+
+            if (data[1] == 1)
+                InputData.isRightButtonCliked = true;
+            else
+                InputData.isRightButtonCliked = false;
+
+            //calibration
+            InputData.Rx = data[2] / 100f;
+            InputData.Ry = data[3] / 100f;
+            InputData.Rz = data[4] / 100f;
+
+            Cube.transform.rotation = Quaternion.Euler(-InputData.Rx, -InputData.Rz, InputData.Ry); //데이터 값에 맞춰서 Cube 회전
+
+            #region 주석
+            //Debug.Log("Left = "+ InputData.isLeftButtonCliked + ", Right = " + InputData.isRightButtonCliked + ", Rx = " + InputData.Rx + ", Ry = " + InputData.Ry + ", Rz = " + InputData.Rz);
+            //InputData.Ax = data[2];
+            //InputData.Ay = data[3];
+            //InputData.Az = data[4];
+
+            //InputData.Gx = data[5];
+            //InputData.Gy = data[6];
+            //InputData.Gz = data[7];
+
+            //Calc_Rotation();
+            #endregion
+
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        sp.Close(); // 프로그램 종료시 포트 닫기
+    }
+
+    #region 주석
+    //void Calc_Rotation()
+    //{
+    //    int ax = data[2], ay = data[3], az = data[4];
+    //    int gx = data[5], gy = data[6], gz = data[7];
+
+    //    double dt;
+    //    double Accel_Angle_x, Accel_Angle_y;
+
+    //    const double RADIANS_TO_DEGREES = 180 / Math.PI;
+    //    const double ALPHA = 0.85;
+
+    //    dt = Time.deltaTime;
+
+    //    double yz = Math.Sqrt(Math.Pow(ay, 2) + Math.Pow(az,2));
+    //    Accel_Angle_y = Math.Atan(-ax / yz) * RADIANS_TO_DEGREES * 90f / 77f;
+
+
+    //    double xz = Math.Sqrt(Math.Pow(ax, 2) + Math.Pow(az, 2));
+    //    Accel_Angle_x = Math.Atan(ay / xz) * RADIANS_TO_DEGREES * 90f / 77f;
+
+
+    //    Gyro_Angle_x += (gx-100) * dt / 100;
+    //    //Gyro_Angle_y += gy * dt / 100;
+    //    //Gyro_Angle_z += gz * dt / 100;
+
+    //    tmp_angle_x = InputData.Rx + (gx - 100) * dt /100;
+    //    tmp_angle_y = InputData.Ry + (gy - 15) * dt / 100;
+    //    tmp_angle_z = InputData.Rz + (gz - 47.9) * dt / 100;
+
+    //    InputData.Rx = ALPHA * tmp_angle_x + (1.0 - ALPHA) * Accel_Angle_x;
+    //    InputData.Ry = ALPHA * tmp_angle_y + (1.0 - ALPHA) * Accel_Angle_y;
+    //    InputData.Rz = tmp_angle_z;
+
+    //    Debug.Log("Rx = "+ InputData.Rx + ", Ry = " + InputData.Ry + " , Rz = " + InputData.Rz) ;
+    //}
+    #endregion
+}
+
+
+
+public static class InputData //받아올 Data
+{
+    public static int Ax, Ay, Az;
+    public static int Gx, Gy, Gz;
+
+    public static float Rx, Ry, Rz;
+    public static bool isLeftButtonCliked, isRightButtonCliked;
+}
---
+++ Shooting Range/Assets/Scripts/GameManager.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public GameObject Gun;
+    public GameObject Bullet;
+    public GameObject Aim;
+    public Rigidbody GunRB;
+    public Text Score;
+    public Text timeText;
+    public int ScorePoint;
+    public int plusScore;
+    public Vector3 force;
+    public bool isShoot;
+    private float time;
+
+    private void Awake()
+    {
+        time = 5f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GunRB = Gun.GetComponent<Rigidbody>();
+        ScorePoint = 0;
+        plusScore = 0;
+        isShoot = false;
+        force = new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    
+        FixShooting();
+        StartCoroutine(Shooting());
+    }
+
+    IEnumerator Shooting()
+    {
+        if (Input.GetMouseButtonDown(1) && Camera.main.fieldOfView != 20) //줌인
+        {
+            Vector3 position = new Vector3(-55f, 5.1f, 6.5f);
+            Vector3 rotation = new Vector3(-2f, 180f, 0f);
+            Debug.Log("Right Mouse Button 1");
+            Camera.main.fieldOfView = 20;
+            Gun.transform.LeanMoveLocal(position, 0.3f);           
+        }
+        else if (Input.GetMouseButtonDown(1) || isShoot == true) //총 쐈을때
+        {
+            Vector3 position = new Vector3(-56.72f, 4.86f, 5.08f);
+            Vector3 rotation = new Vector3(-6.599f, 172.567f, 0.266f);
+            Debug.Log("Right Mouse Button 2");
+            Camera.main.fieldOfView = 50;
+            Gun.transform.LeanMoveLocal(position, 0.3f);
+
+            force = new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), 0f);
+            Debug.Log(force.x +" "+ force.y +" "+ force.z);
+            isShoot = false;
+            time = 5f;
+        }
+
+        if (Camera.main.fieldOfView == 20)
+        {
+            if (time > 0)
+                time -= Time.deltaTime;
+            else
+            {
+                isShoot = true;
+                Shooting();
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                ShootBullet();
+
+                Gun.gameObject.GetComponent<Animator>().SetBool("IsShoot", true);
+                yield return new WaitForSeconds(0.2f);
+                Gun.gameObject.GetComponent<Animator>().SetBool("IsShoot", false);
+                yield return new WaitForSeconds(0.4f);
+                isShoot = true;
+            }
+            GunRB.AddForce(force);
+
+            Vector3 pos = Camera.main.WorldToViewportPoint(Gun.transform.position);
+
+            if (pos.x < 0f) pos.x = 0f;
+
+            if (pos.x > 1f) pos.x = 1f;
+
+            //if (pos.y < 0f) pos.y = 0f;
+
+            //if (pos.y > 1f) pos.y = 1f;
+
+            Gun.transform.position = Camera.main.ViewportToWorldPoint(pos);
+        }
+        else
+        {
+            GunRB.velocity = Vector3.zero;
+            GunRB.angularVelocity = Vector3.zero;
+        }
+
+        timeText.text = "시간 : " + Mathf.Ceil(time).ToString();
+        Score.text = "점수 : " + ScorePoint + " (+" + plusScore + ")";
+    }
+
+    void FixShooting()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            GunRB.AddForce(0f, 0.2f, 0f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            GunRB.AddForce(0f, -0.2f, 0f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GunRB.AddForce(0.2f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GunRB.AddForce(-0.2f, 0f, 0f);
+        }
+    }
+
+    void ShootBullet()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Bullet.transform.position, Bullet.transform.forward, out hit))
+        {
+            Debug.Log(hit.point);
+            float ShootPoint = Vector3.Distance(Aim.transform.position, hit.point);
+            if (ShootPoint < 0.022f)
+            {
+                Debug.Log(ShootPoint);
+                Debug.Log("10점 입니다.");
+                ScorePoint += 10;
+                plusScore = 10;
+            }
+            else if (ShootPoint > 0.022f && ShootPoint < 0.04f)
+            {
+                Debug.Log(ShootPoint);
+                Debug.Log("9점 입니다.");
+                ScorePoint += 9;
+                plusScore = 9;
+            }
+            else if (ShootPoint > 0.04f && ShootPoint < 0.06f)
+            {
+                Debug.Log(ShootPoint);
+                Debug.Log("8점 입니다.");
+                ScorePoint += 8;
+                plusScore = 8;
+            }
+            else if (ShootPoint > 0.06f && ShootPoint < 0.08f)
+            {
+                Debug.Log(ShootPoint);
+                Debug.Log("7점 입니다.");
+                ScorePoint += 7;
+                plusScore = 7;
+            }
+            else if (ShootPoint > 0.08f && ShootPoint < 0.1f)
+            {
+                Debug.Log(ShootPoint);
+                Debug.Log("6점 입니다.");
+                ScorePoint += 6;
+                plusScore = 6;
+            }
+            else if (ShootPoint > 0.1f && ShootPoint < 0.12f)
+            {
+                Debug.Log(ShootPoint);
+                Debug.Log("5점 입니다.");
+                ScorePoint += 5;
+                plusScore = 5;
+            }
+            else if (ShootPoint > 0.12f && ShootPoint < 0.14f)
+            {
+                Debug.Log(ShootPoint);
+                Debug.Log("4점 입니다.");
+                ScorePoint += 4;
+                plusScore = 4;
+            }
+            else if (ShootPoint > 0.14f && ShootPoint < 0.16f)
+            {
+                Debug.Log(ShootPoint);
+                Debug.Log("3점 입니다.");
+                ScorePoint += 3;
+                plusScore = 3;
+            }
+            else if (ShootPoint > 0.16f && ShootPoint < 0.18f)
+            {
+                Debug.Log(ShootPoint);
+                Debug.Log("2점 입니다.");
+                ScorePoint += 2;
+                plusScore = 2;
+            }
+            else if (ShootPoint > 0.18f && ShootPoint < 0.2f)
+            {
+                Debug.Log(ShootPoint);
+                Debug.Log("1점 입니다.");
+                ScorePoint += 1;
+                plusScore = 1;
+            }
+            else
+            {
+                Debug.Log(ShootPoint);
+                Debug.Log("못마춰써");
+                plusScore = 0;
+            }
+            //force = new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), 0f);
+            //Debug.Log(force);
+
+        }
+    }
+}
